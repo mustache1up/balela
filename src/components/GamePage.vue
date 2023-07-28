f07d329e224f852f4f0891579b8ce2e2848bfc56
mustache1up <daniel.sp@msn.com>
2023-07-28 11:06:19 -0300

    add timer on GamePage

--- src/components/GamePage.vue
+++ src/components/GamePage.vue
@@ -1,23 +1,27 @@
-<!-- GamePage.vue -->
 <template>
   <div>
     <h1>Rodada do Jogo</h1>
     <p>Mediador da rodada: {{ mediatorName }}</p>
     <p>Palavra: {{ word }}</p>
-
-    <!-- Se for o mediador, mostrar a definição -->
     <p v-if="isMediator">Definição: {{ definition }}</p>
 
-    <!-- Campo de texto para jogadores escreverem suas definições (aparece somente quando não é o mediador) -->
     <div v-if="!isMediator">
       <label for="playerDefinition">Sua definição:</label>
       <input type="text" id="playerDefinition" v-model="playerDefinition" />
       <button @click="submitDefinition">Enviar Definição</button>
     </div>
+
+    <!-- Mostrar a contagem regressiva -->
+    <div v-if="!isMediator && timeLeft > 0">
+      <p>Tempo restante: {{ timeLeft }} segundos</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref, computed, watch } from "vue";
+import router from "../router"; // Certifique-se de corrigir o caminho para o arquivo router.js, se necessário
+
 export default {
   props: {
     mediatorName: String,
@@ -25,22 +29,50 @@ export default {
     definition: String,
     currentPlayer: String,
   },
-  data() {
+  setup(props) {
+    const playerDefinition = ref("");
+    const timeLimit = 30; // Tempo limite em segundos
+    const timeLeft = ref(timeLimit);
+
+    function submitDefinition() {
+      // Lógica para enviar a definição do jogador...
+    }
+
+    const isMediator = computed(() => {
+      return props.mediatorName === props.currentPlayer;
+    });
+
+    // Inicia a contagem regressiva quando o componente for criado
+    const timer = setInterval(() => {
+      timeLeft.value--;
+      if (timeLeft.value <= 0) {
+        clearInterval(timer);
+        // Redireciona para a VotingPage quando o tempo acabar
+        redirectToVotingPage();
+      }
+    }, 1000);
+
+    // Redireciona para a VotingPage
+    function redirectToVotingPage() {
+      router.push("/voting");
+    }
+
+    // Quando o componente for destruído, limpa o temporizador
+    watch(
+      () => timeLeft.value,
+      (newValue) => {
+        if (newValue <= 0) {
+          clearInterval(timer);
+        }
+      }
+    );
+
     return {
-      playerDefinition: "", // Campo de texto para a definição do jogador
+      playerDefinition,
+      submitDefinition,
+      isMediator,
+      timeLeft,
     };
-  },
-  computed: {
-    isMediator() {
-      return this.mediatorName === this.currentPlayer;
-    },
-  },
-  methods: {
-    submitDefinition() {
-      // Implementar a lógica para enviar a definição do jogador para o servidor ou atualizar o estado do jogo
-      // Por exemplo, você pode emitir um evento para notificar o componente pai sobre a definição do jogador
-      // this.$emit('submit-definition', this.playerDefinition);
-    },
   },
 };
 </script>
